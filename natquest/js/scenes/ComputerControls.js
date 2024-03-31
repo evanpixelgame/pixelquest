d0ac7dc895e1f4961ad3b2b7cc0bcbfa8cd76258
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 14:53:03 -0400

    Update ComputerControls.js

--- natquest/js/scenes/ComputerControls.js
+++ natquest/js/scenes/ComputerControls.js
@@ -69,7 +69,9 @@ update(time, delta) {
     }
 
     // Set the velocity of the player sprite
-    this.player.body.setVelocity(velocityX, velocityY);
+  this.player.body.velocity.x = velocityX;
+this.player.body.velocity.y = velocityY;
+   // this.player.setVelocity(velocityX, velocityY);
   console.log(this.player);
     console.log(this.player.body);
     console.log(this.player.velocity);
